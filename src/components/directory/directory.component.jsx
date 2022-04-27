0e70bb94ee10b8deda21e19a07b85da9e7483475
lairton33 <developmentacc569@gmail.com>
2022-04-27 11:19:54 -0300

    Updated classNames, and the components name to avoid css crashing.

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,14 +1,12 @@
-import CategoryItem from "../category-item/category-item.component"
-import "./directories.styles.scss"
+import DirectoryItem from "../directory-item/directory-item.component";
+import "./directories.styles.scss";
 
 const Directory = ({ categories }) => {
-  
   return (
-    <div className='directory-container'>
-      {categories.map(((category) => (
-          <CategoryItem key={category.id} category={category}/>
-      )))}
-      
+    <div className="directory-container">
+      {categories.map((category) => (
+        <DirectoryItem key={category.id} category={category} />
+      ))}
     </div>
   );
 };
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,13 +1,16 @@
 import { useContext } from "react/";
-
 import { Routes, Route } from "react-router-dom";
-import "./shop.styles.scss";
+
 import CategoriesPreview from "../categories-preview/categories-preview.component";
+import Category from "../category/category.component";
+
+import "./shop.styles.scss";
 
 const Shop = () => {
   return (
     <Routes>
       <Route index element={<CategoriesPreview />}></Route>
+      <Route path=":category" element={<Category />}></Route>
     </Routes>
   );
 };
